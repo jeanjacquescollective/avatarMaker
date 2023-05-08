fde6b7beddd7385ae73eef8b9149633ff77e143a
jannes lambrecht <janneslambrecht@hotmail.com>
2023-05-08 11:29:50 +0200

    simple form

---
+++ simple-demo-form/js/form.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('signup-form');
+const userList = document.getElementById('user-list');
+
+// Load existing users from local storage
+let users = JSON.parse(localStorage.getItem('users')) || [];
+
+// Render existing users on page load
+users.forEach(user => {
+  const li = document.createElement('li');
+  li.textContent = `${user.name} (${user.email})`;
+  userList.appendChild(li);
+});
+
+// Handle form submission
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  // Get form values
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  // Create new user object
+  const user = { name, email, password };
+
+  // Add user to list on page
+  const li = document.createElement('li');
+  li.textContent = `${user.name} (${user.email})`;
+  userList.appendChild(li);
+
+  // Add user to local storage
+  users.push(user);
+  localStorage.setItem('users', JSON.stringify(users));
+
+  // Clear form
+  form.reset();
+});
